e51e838a0220849bef9e39c0983dc3097e7932df
Deepak <deepak@Deepaks-MacBook-Pro.local>
2019-12-12 15:27:20 +0530

    collation removed

--- shortener/init.sql
+++ shortener/init.sql
@@ -9,5 +9,5 @@ CREATE TABLE `url` (
   `custom` varchar(32) NOT NULL DEFAULT '',
   `original` varchar(512) NOT NULL DEFAULT '',
   PRIMARY KEY (`custom`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
